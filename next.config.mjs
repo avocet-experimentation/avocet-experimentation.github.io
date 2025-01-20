47ecec2c6b8176b5842d2acc82edb0a35a34dec6
Adnan Shihabi <adnan@ashihabi.me>
2025-01-20 00:49:32 -0800

    Add next configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+  assetPrefix: isProd ? '/avocet.github.io' : '',
+  basePath: isProd ? '/avocet.github.io' : '',
   output: 'export',
   distDir: 'dist',
   images: {

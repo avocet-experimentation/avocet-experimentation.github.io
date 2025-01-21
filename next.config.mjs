41a3e37e2def6433bdf8bdf25aa2728769dc21a0
Adnan Shihabi <adnan@ashihabi.me>
2025-01-20 16:20:36 -0800

    env and misc configurations

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,16 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  assetPrefix: isProd ? '/avocet.github.io' : '',
-  basePath: isProd ? '/avocet.github.io' : '',
+  assetPrefix: isProd ? '/dist' : '',
+  basePath: isProd ? '/dist' : '',
   output: 'export',
   distDir: 'dist',
   images: {
     unoptimized: true,
   },
+  env: {
+    BASE_PATH: process.env.BASE_PATH,
+  }
 };
 
 export default nextConfig;
